9e24574e4b42860d855f682cd9264444db50de06
Marc Valles Casares <13a1911@gmail.com>
2019-11-07 16:46:46 +0100

    logout implemented on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
+import { withAuth } from '../Context/AuthContext';
 import '../styles/Navbar.css';
 
-const Navbar = () => {
+const Navbar = props => {
+  const { handleLogout } = props;
   return (
     <div className="navbar">
       <img src="https://randomuser.me/api/portraits/men/18.jpg" className="avatar" alt="avatar"></img>
       <ul className="navbar-menu">
         <li className="navbar-item">Profile</li>
         <li className="navbar-item">Favourite Places</li>
-        <li className="navbar-item">Logout</li>
+        <li className="navbar-item" onClick={handleLogout}>
+          Logout
+        </li>
       </ul>
     </div>
   );
 };
 
-export default Navbar;
+export default withAuth(Navbar);
